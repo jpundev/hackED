12cf50526d9937a3cbfce3e73fdb81cb295ba98f
Hee Soo Park <heesoopark@Hees-MacBook-Pro.local>
2019-01-19 16:57:43 -0700

    new

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -3,8 +3,11 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,7 +19,30 @@ public class RegisterActivity extends AppCompatActivity {
         Button confirm_button = findViewById(R.id.register_button);
         confirm_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View V) {
-                finish();
+                final EditText edit =  (EditText) findViewById(R.id.enter_password);
+                String passwordString = (String) edit.getText().toString();
+
+                boolean isAtLeast8   = passwordString.length() >= 8;
+                boolean hasUppercase = !passwordString.equals(passwordString.toLowerCase());
+                boolean hasLowercase = !passwordString.equals(passwordString.toUpperCase());
+                boolean hasSpecial   = !passwordString.matches("[A-Za-z0-9 ]*");
+
+                if(!isAtLeast8) {
+                    Toast.makeText(RegisterActivity.this, "Password must be at least 8 characters",
+                            Toast.LENGTH_LONG).show();
+                }else if(!hasUppercase) {
+                    Toast.makeText(RegisterActivity.this, "Password must contain Uppercase",
+                            Toast.LENGTH_LONG).show();
+                }else if(!hasLowercase) {
+                    Toast.makeText(RegisterActivity.this, "Password must contain Lowercase",
+                            Toast.LENGTH_LONG).show();
+                }else if(!hasSpecial) {
+                    Toast.makeText(RegisterActivity.this, "Password must contain Special characters",
+                            Toast.LENGTH_LONG).show();
+                }
+                else{
+                    finish();
+                }
             }
         });
 
